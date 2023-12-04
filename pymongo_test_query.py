57ce56d7123c66c4c4e4b1109c038b28459b9bec
Vio Szabo <vio@vioszabo.se>
2023-12-04 11:58:43 +0100

    testing queries and connection to Atlas

---
+++ pymongo_test_query.py
@@ -0,0 +1,16 @@
+# Get the database using the method we defined in pymongo_test_insert file
+from pandas import DataFrame
+from pymongo_get_database import get_database
+dbname = get_database()
+ 
+# Retrieve a collection named "user_1_items" from database
+collection_name = dbname["user_1_items"]
+ 
+item_details = collection_name.find()
+for item in item_details:
+   # This does not give a very readable output
+  # convert the dictionary objects to dataframe
+  items_df = DataFrame(item_details)
+
+# see the magic
+print(items_df)
